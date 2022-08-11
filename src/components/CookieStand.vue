5ca93010ca7ffab690979b4f05ffc91628211d67
Brady Davenport <brady.davenport@pm.me>
2022-08-10 20:14:10 -0600

    styling

--- src/components/CookieStand.vue
+++ src/components/CookieStand.vue
@@ -6,96 +6,82 @@
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Akshar:wght@300&display=swap" />
 
 <template>
-  <h1 class="highlights">Salmon Cookies</h1>
-  <img src="img/salmon.png" width="600" />
-  <nav class="text">
-    <ul>
-      <li><a href="sales.html">Sales</a></li>
-    </ul>
-  </nav>
-  <section class="center">
-    <div>
-      <img src="img/chinook.jpg" alt="Chinook fish" height="250" width="300" />
-    </div>
 
-    <div>
-      <img
-        src="img/cutter.jpeg"
-        alt="Fish cookie cutter"
-        height="250"
-        width="300"
-      />
-    </div>
-  </section>
-
-  <section>
-    <div>
-      ***********Sales Table here***************
-      <!-- <SalesTable /> -->
-    </div>
-
-    <section class="center">
-      <div>
-        <img
-          src="img/family.jpg"
-          alt="stock photo of family at table with cookies and milk"
-          height="250"
-          width="300"
-        />
-      </div>
-      <div>
-        <img
-          src="img/fish.jpg"
-          alt="animated salmon"
-          height="250"
-          width="300" />
-      </div>
-    </section>
-
+  <body>
+    <h1 class="highlights">Salmon Cookies</h1>
+    <img src="img/salmon.png" width="600" />
     <div>
       <ul class="text">
-        Hours
+        <h3>Hours</h3>
         <li>Monday thru Saturday - 6am to 8pm</li>
         <li>Sunday - Closed</li>
       </ul>
     </div>
-
     <section class="center">
-      <div>
-        <img
-          src="img/frosted-cookie.jpg"
-          alt="frosted salmon decorated cookie"
-          height="250"
-          width="300"
-        />
-      </div>
-      <div>
-        <img src="img/shirt.jpg" alt="shirt" height="250" width="300" />
+      <div class="pics">
+        <img src="img/chinook.jpg" alt="Chinook fish" height="250" width="300" />
+        <img src="img/cutter.jpeg" alt="Fish cookie cutter" height="250" width="300" />
       </div>
     </section>
 
-    <div>
-      <ul class="text">
-        Contact Us
-        <li>cookiestandemail@cookiestand.com</li>
-        <li>000-867-5309</li>
-      </ul>
-    </div>
-  </section>
+    <section>
+      <div>
+        ***********Sales Table here***************
+        <!-- <SalesTable /> -->
+      </div>
+
+      <section class="center">
+        <div class="pics">
+          <img src="img/family.jpg" alt="stock photo of family at table with cookies and milk" height="250"
+            width="300" />
+          <img src="img/fish.jpg" alt="animated salmon" height="250" width="300" />
+        </div>
+      </section>
+
+      <div>
+        <ul class="text">
+          Contact Us
+          <li>cookiestandemail@cookiestand.com</li>
+          <li>000-867-5309</li>
+        </ul>
+      </div>
+
+      <section class="center">
+        <div class="pics">
+          <img src="img/frosted-cookie.jpg" alt="frosted salmon decorated cookie" height="250" width="300" />
+          <img src="img/shirt.jpg" alt="shirt" height="250" width="300" />
+        </div>
+      </section>
+    </section>
+  </body>
 </template>
 
 <style scoped>
-template {
+body {
+  width: inherit;
   display: flex;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
+  align-content: center;
+  gap: 20px;
 }
+
+.pics {
+  display: flex;
+  flex-direction: row;
+  align-content: space-between;
+  gap: 30px;
+}
+
 .highlights {
   font-family: "Akshar", sans-serif;
 }
+
 .text {
   font-family: Georgia, "Times New Roman", Times, serif;
 }
+
 .center {
   display: flex;
   flex-direction: row;
